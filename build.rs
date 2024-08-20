f95ea8ecc9e78c525dd848302e1c28f1c8c29966
Lazy Panda <lepidodendraceae@gmail.com>
2024-08-20 15:34:31 +0800

    epic: 0.1.3

--- build.rs
+++ build.rs
@@ -1,86 +1,80 @@
-use std::env;
-use std::io::Error;
-use std::path::*;
-use std::process::*;
+use std::{env, fs, path::Path, process::Command};
 
-fn join(a: &str, b: &str) -> PathBuf {
-    Path::new(a).join(b)
+use anyhow::anyhow;
+
+fn join(root: &str, next: &str) -> anyhow::Result<String> {
+    Ok(Path::new(root)
+        .join(next)
+        .to_str()
+        .ok_or_else(|| anyhow::anyhow!("Failed to path into string."))?
+        .to_string())
 }
 
-fn split(path: &Path) -> (String, String) {
-    let name = path.file_stem().unwrap().to_str().unwrap().to_string();
-    let dir = path.parent().unwrap().to_str().unwrap().to_string();
-
-    (dir,
-     name.starts_with("lib")
-         .then(|| name.replacen("lib", "", 1))
-         .unwrap_or(name))
+fn is_exsit(dir: &str) -> bool {
+    fs::metadata(dir).is_ok()
 }
 
-fn get_lib_name(key: &str, long: bool) -> String {
+fn exec(command: &str, work_dir: &str) -> anyhow::Result<String> {
+    let output = Command::new(if cfg!(target_os = "windows") {
+        "powershell"
+    } else {
+        "bash"
+    })
+    .arg(if cfg!(target_os = "windows") {
+        "-command"
+    } else {
+        "-c"
+    })
+    .arg(if cfg!(target_os = "windows") {
+        format!("$ProgressPreference = 'SilentlyContinue';{}", command)
+    } else {
+        command.to_string()
+    })
+    .current_dir(work_dir)
+    .output()?;
+    if !output.status.success() {
+        Err(anyhow!("{}", unsafe {
+            String::from_utf8_unchecked(output.stderr)
+        }))
+    } else {
+        Ok(unsafe { String::from_utf8_unchecked(output.stdout) })
+    }
+}
+
+fn library_name(complete: bool) -> String {
     let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
 
-    let ext = if cfg!(windows) { "lib" } else { "a" };
-    let flag = if cfg!(windows) { "" } else { "lib" };
-    let name = format!("{}-{}-{}", key, os, arch);
-    if long {
+    let ext = if cfg!(target_os = "windows") { "lib" } else { "a" };
+    let flag = if cfg!(target_os = "windows") { "" } else { "lib" };
+    let name = format!("yuv-{}-{}", os, arch);
+    if complete {
         format!("{}{}.{}", flag, name, ext)
     } else {
         name
     }
 }
 
-#[cfg(target_os = "windows")]
-fn exec(cmd: &str, work_dir: &str) -> Result<ExitStatus, Error> {
-    Command::new("powershell").args(["-command", cmd])
-                              .current_dir(work_dir)
-                              .status()
-}
-
-#[cfg(not(target_os = "windows"))]
-fn exec(command: &str, work_dir: &str) -> Result<ExitStatus, Error> {
-    Command::new("bash").arg("-c")
-                        .arg(command)
-                        .current_dir(work_dir)
-                        .status()
-}
-
-fn download(name: &str) -> (String, String) {
+fn main() -> anyhow::Result<()> {
+    println!("cargo:cargo:rerun-if-env-changed=./src");
+    
     let repository = env::var("CARGO_PKG_REPOSITORY").unwrap();
     let version = env::var("CARGO_PKG_VERSION").unwrap();
     let output = env::var("OUT_DIR").unwrap();
 
-    let lib_name = get_lib_name(name, true);
-    let path = join(&output, &lib_name);
-    if !path.exists() {
-        let url = &format!("{}/releases/download/v{}/{}", repository, version, lib_name);
-        let path = path.to_str().unwrap();
-        if cfg!(windows) {
-            exec(&format!("Invoke-WebRequest -Uri {} -OutFile {}", url, path),
+    if !is_exsit(&join(&output, &library_name(true))?) {
+        let url = format!("{}/releases/download/v{}/{}", repository, version, library_name(true));
+        if cfg!(target_os = "windows") {
+            exec(&format!("Invoke-WebRequest -Uri {}", url),
                  &output)
         } else {
-            exec(&format!("curl -f -L -o {} {}", path, url), &output)
+            exec(&format!("wget {}", url), &output)
         }.expect("There is no precompiled binary library file in git \
                 releases, please try to compile it yourself according to the \
                 README, see https://github.com/colourful-rtc/libyuv-rs");
     }
 
-    split(&path)
-}
-
-fn main() {
-    for name in ["YUV_LIBRARY_PATH"] {
-        println!("cargo:cargo:rerun-if-env-changed={}", name);
-        if let Ok(path) = env::var(name) {
-            println!("cargo:rerun-if-changed={}", path);
-        }
-    }
-
-    let (yuv_lib_path, yuv_lib_name) =
-        env::var("YUV_LIBRARY_PATH").map(|path| split(Path::new(&path)))
-                                    .unwrap_or_else(|_| download("yuv"));
-
-    println!("cargo:rustc-link-lib={}", yuv_lib_name);
-    println!("cargo:rustc-link-search=all={}", yuv_lib_path);
+    println!("cargo:rustc-link-search=all={}", output);
+    println!("cargo:rustc-link-lib={}", library_name(false));
+    Ok(())
 }
